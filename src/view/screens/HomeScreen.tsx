9008c7c187d55aa1141811f85719e67644744353
Gabriel <gabee.dlc@gmail.com>
2022-09-02 13:47:10 -0300

    Edit HomeScreen

--- src/view/screens/HomeScreen.tsx
+++ src/view/screens/HomeScreen.tsx
@@ -1,18 +1,74 @@
-import { StyleSheet } from 'react-native';
-import { Text, View } from '../components/Themed';
+import React from "react";
+import { Alert, Button, FlatList, Image, StyleSheet, View, Text } from "react-native";
+
+const DATA = [
+  {
+    id: 1,
+    name: 'Jorch', 
+    payed: true,
+  },
+  {
+    id: 2,
+    name: 'Mumo', 
+    payed: false,
+  },
+  {
+    id: 3,
+    name: 'Emilio', 
+    payed: true,
+  },
+  {
+    id: 4,
+    name: 'Gabe', 
+    payed: false,
+  },
+  {
+    id: 5,
+    name: 'Marian', 
+    payed: true,
+  },
+]
+
+
 
 const HomeScreen = () => (
   <View style={styles.container}>
-    <Text>Hola Mundo</Text>
+    <Text>Equipo A</Text>
+    <Text>Control de pagos | Fecha 1</Text>
+    <View>
+      <View>
+        <Text>#</Text>
+        <Text>Nombre</Text>
+      </View>
+      {/* <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      /> */}
+    </View>
+    <View>
+      <Button
+        title="back button"
+        onPress={() => Alert.alert("back button pressed")}
+      >
+        <Image source={require("../../../assets/images/back-arrow.png")} />
+      </Button>
+      <Button
+        title="confirm button"
+        onPress={() => Alert.alert("confirm button pressed")}
+      >
+        <Image source={require("../../../assets/images/confirm-arrow.png")} />
+      </Button>
+    </View>
   </View>
-)
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
-export default HomeScreen
+export default HomeScreen;
